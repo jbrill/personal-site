fc32855fb810638e87213579e238b55e8da75bf5
jlbrill <jbrill95@gmail.com>
2019-06-03 19:44:22 -0400

    Move up a subdir

---
+++ src/components/About.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <Base>
+      <p class="about__contain about__contain__header">👋 🌎</p>
+      <p class="about__contain">
+        I am an artist and engineer looking to impact the way in which people
+        interact with their environments
+      </p>
+      <p class="about__contain">
+        I specialize in full stack web development, and am currently redefining
+        conversational AI for the world with
+        <a
+          class="about__contain__outbound-link"
+          target="_blank"
+          href="https://clinc.com/"
+          >Clinc</a
+        >
+      </p>
+      <p class="about__contain">
+        I plan on obtaining a graduate degree in Human-Computer Interaction in
+        the near future to better understand the
+        <span class="about__contain__italic">why</span> rather than the
+        <span class="about__contain__italic">how</span>
+      </p>
+    </Base>
+  </div>
+</template>
+
+<script>
+import Base from "./Base.vue";
+
+export default {
+  name: "About",
+  components: {
+    Base
+  }
+};
+</script>
+
+<style>
+.about__contain__outbound-link:hover {
+  background-color: coral;
+}
+.about__contain {
+  margin-bottom: 10%;
+}
+.about__contain__italic {
+  font-style: italic;
+}
+</style>
---
+++ src/components/Base.vue
@@ -0,0 +1,323 @@
+<template>
+  <div>
+    <div class="content__contain">
+      <div class="content__contain__left">
+        <div class="profile__contain">
+          <router-link to="/"
+            ><div id="orangeContain">
+              <img :src="`${publicPath}avatar.svg`" /></div
+          ></router-link>
+          <router-link to="/"
+            ><h1 class="title__contain">JASON BRILL</h1></router-link
+          >
+        </div>
+        <ul class="menu__contain">
+          <router-link to="/about" tag="li">WHO AM I</router-link>
+          <!-- <router-link to="/work" tag="li">WORK</router-link> -->
+
+          <router-link to="/photography" tag="li">PHOTOGRAPHY</router-link>
+          <!-- <router-link to="/music" tag="li">MUSIC</router-link> -->
+        </ul>
+      </div>
+      <div class="content__contain__right">
+        <slot></slot>
+        <div class="bottom__feed__contain">
+          <div class="outbound__contain">
+            <span class="outbound__contain__contact"
+              ><a href="mailto:jbrill95@gmail.com">EMAIL</a></span
+            >
+            //
+            <span class="outbound__contain__contact"
+              ><a
+                href="https://drive.google.com/file/d/134qFAeXeUm6T9gdjxfHFVPGYUuWNcB6m/view?usp=sharing"
+                target="_blank"
+                >RESUME</a
+              ></span
+            >
+            //
+            <span class="outbound__contain__contact"
+              ><a href="https://www.soundcloud.com/brillay/" target="_blank"
+                >SOUNDCLOUD</a
+              ></span
+            >
+            //
+            <span class="outbound__contain__contact"
+              ><a href="https://github.com/jbrill" target="_blank"
+                >GITHUB</a
+              ></span
+            >
+          </div>
+          <div class="lower__copyright">
+            MADE WITH ❤️ | COPYRIGHT © JASON BRILL
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Base",
+  data() {
+    return {
+      publicPath: process.env.BASE_URL
+    };
+  }
+};
+</script>
+
+<style>
+html {
+  font-family: "Proxima Nova";
+  width: 100%;
+  height: 100%;
+}
+
+body {
+  background-color: white;
+  width: 80%;
+  height: 100%;
+  margin: auto;
+  margin-top: 4%;
+  animation-name: fadeInOpacity;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in;
+  animation-duration: 1s;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+#orangeContain {
+  top: 0;
+  right: 0;
+  width: 40%;
+  height: auto;
+  background-color: #ffa531;
+}
+
+#orangeContain:hover {
+  cursor: alias;
+}
+
+.content__contain {
+  width: 100%;
+  animation-name: fadeInOpacity;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in;
+  animation-duration: 1s;
+  transition: opacity 0.3s ease-out;
+  -moz-transition: opacity 0.3s ease-out;
+  -webkit-transition: opacity 0.3s ease-out;
+  -o-transition: opacity 0.3s ease-out;
+}
+
+.title__contain {
+  font-weight: 400;
+  font-size: 100%;
+  letter-spacing: 0.3rem;
+  width: 40%;
+  animation-name: fadeInOpacity;
+  animation-iteration-count: 1;
+  animation-timing-function: ease-in;
+  animation-duration: 1s;
+  transition: opacity 0.3s ease-out;
+  -moz-transition: opacity 0.3s ease-out;
+  -webkit-transition: opacity 0.3s ease-out;
+  -o-transition: opacity 0.3s ease-out;
+}
+
+.title__contain:hover {
+  cursor: pointer;
+  opacity: 0.5;
+  transition: opacity 0.3s ease-out;
+  -moz-transition: opacity 0.3s ease-out;
+  -webkit-transition: opacity 0.3s ease-out;
+  -o-transition: opacity 0.3s ease-out;
+}
+
+.content__contain__left {
+  float: left;
+  width: 25%;
+}
+
+.content__contain__right {
+  width: 75%;
+  float: right;
+  font-size: 1.5rem;
+  margin-top: 3rem;
+}
+
+.menu__contain {
+  list-style: none;
+  margin-top: 4rem;
+  width: 100%;
+  margin-bottom: 0;
+  padding-left: 1.2rem;
+}
+
+.menu__contain li {
+  margin-bottom: 2em;
+}
+
+.menu__contain li:hover {
+  cursor: pointer;
+  text-decoration: line-through;
+}
+
+.about__contain {
+  width: 80%;
+  font-weight: lighter;
+  margin-bottom: 2rem;
+}
+
+.about__contain__header {
+  font-style: normal;
+}
+
+.outbound__contain {
+  bottom: 0;
+  right: 2rem;
+  font-size: 0.8rem;
+  padding-bottom: 10px;
+  font-style: oblique;
+  text-align: right;
+}
+
+.outbound__contain__contact:hover {
+  cursor: pointer;
+  background-color: coral;
+}
+
+.outbound__contain a {
+  text-decoration: none;
+  color: black;
+}
+.bottom__feed__contain {
+  position: fixed;
+  right: 20px;
+  bottom: 0;
+  min-width: 20%;
+  /* z-index: -10; */
+}
+
+.lower__copyright {
+  bottom: 0;
+  margin: 0 auto;
+  width: 100%;
+  padding-bottom: 10px;
+  text-align: right;
+  left: 2rem; /* approx */
+  opacity: 0.6;
+  font-size: 0.3rem;
+  font-weight: 100;
+  z-index: -10;
+}
+
+@keyframes fadeInOpacity {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+::selection {
+  background: #ffa531;
+}
+
+#orangeContain {
+  background: linear-gradient(
+    124deg,
+    #ff2400,
+    #e81d1d,
+    #e8b71d,
+    #e3e81d,
+    #1de840,
+    #1ddde8,
+    #2b1de8,
+    #dd00f3,
+    #dd00f3
+  );
+  background-size: 1800% 1800%;
+
+  -webkit-animation: rainbow 60s ease infinite;
+  -z-animation: rainbow 60s ease infinite;
+  -o-animation: rainbow 60s ease infinite;
+  animation: rainbow 60s ease infinite;
+}
+
+@-webkit-keyframes rainbow {
+  0% {
+    background-position: 0% 82%;
+  }
+  50% {
+    background-position: 100% 19%;
+  }
+  100% {
+    background-position: 0% 82%;
+  }
+}
+@-moz-keyframes rainbow {
+  0% {
+    background-position: 0% 82%;
+  }
+  50% {
+    background-position: 100% 19%;
+  }
+  100% {
+    background-position: 0% 82%;
+  }
+}
+@-o-keyframes rainbow {
+  0% {
+    background-position: 0% 82%;
+  }
+  50% {
+    background-position: 100% 19%;
+  }
+  100% {
+    background-position: 0% 82%;
+  }
+}
+@keyframes rainbow {
+  0% {
+    background-position: 0% 82%;
+  }
+  50% {
+    background-position: 100% 19%;
+  }
+  100% {
+    background-position: 0% 82%;
+  }
+}
+
+@media only screen and (max-width: 800px) {
+  .content__contain__left {
+    width: 100%;
+    margin-top: 1rem;
+  }
+  .profile__contain {
+    float: left;
+    width: 30%;
+  }
+  .menu__contain {
+    margin-top: 0;
+    padding-left: 0;
+    text-align: right;
+  }
+  .content__contain__right {
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    margin-top: 0;
+  }
+  .bottom__feed__contain {
+    position: relative;
+  }
+}
+</style>
